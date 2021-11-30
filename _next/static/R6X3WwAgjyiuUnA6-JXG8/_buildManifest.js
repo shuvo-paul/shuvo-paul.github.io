e615243cd0301f035d03cbcc9ccb76c25a7ae60f
shuvo-paul <shuvopaul@yahoo.com>
2021-11-30 22:25:39 +0000

    Deploying to gh-pages from @ shuvo-paul/shuvo-paul.github.io@669e1a9df1c33b473fde57e1f421cd41783f6bd6 🚀

---
+++ _next/static/R6X3WwAgjyiuUnA6-JXG8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-adf9cc7a982764adb5a4.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
