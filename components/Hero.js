7a4584c2f55412b95ec3c49507ed418f41a5eaba
Shuvo <shuvopaul@yahoo.com>
2021-12-01 04:03:15 +0600

    hero added

--- components/Hero.js
+++ components/Hero.js
@@ -1,13 +1,37 @@
 const Hero = () => {
     return (
-        <section className="hero">
+        <section className="hero py-8">
             <div className="container">
                 <div className="max-w-5xl mx-auto">
-                    <div className="text-white text-opacity-70 font-bold text-6xl">
-                        Hello,
-                    </div>
-                    <div className="hero__title text-opacity-90 text-white text-4xl">
-                        I am a self motivated web developer :)
+                    <div className="flex flex-wrap">
+                        <div className="flex-1 px-4">
+                            <div className="max-w-[512px]">
+                                <div className="text-white text-opacity-70 font-bold text-6xl mb-6">
+                                    Hello,
+                                </div>
+                                <h1 className="hero__title text-opacity-90 text-white text-4xl mb-4">
+                                    I am a self motivated web developer :)
+                                </h1>
+                                <p className="text-white text-opacity-70 mb-4">
+                                    It's fun to me to create things for the web. I always love to play with team. So that in the very beging of my dev carrier I founded a team called "LazyCoders". The main thought behind the name was we don't like <strong className="text-white/80">redundency</strong>.
+                                </p>
+                                <p className="text-white text-opacity-70">Here are few techs I love to work with:</p>
+                                <ul className="tech-list text-white text-opacity-70 grid grid-cols-2 gap-2 mt-6">
+                                    <li className="before:content-['➪'] before:text-primary-500 before:mr-2 font-light">ReactJs</li>
+                                    <li className="before:content-['➪'] before:text-primary-500 before:mr-2 font-light">NextJs</li>
+                                    <li className="before:content-['➪'] before:text-primary-500 before:mr-2 font-light">ExpressJS</li>
+                                    <li className="before:content-['➪'] before:text-primary-500 before:mr-2 font-light">SilverStripe</li>
+                                    <li className="before:content-['➪'] before:text-primary-500 before:mr-2 font-light">Wordpress</li>
+                                    <li className="before:content-['➪'] before:text-primary-500 before:mr-2 font-light">MongoDB</li>
+                                    <li className="before:content-['➪'] before:text-primary-500 before:mr-2 font-light">MySQL</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="flex-shrink px-4">
+                            <div className="w-[224px] rounded-lg overflow-hidden mt-16 mr-6">
+                                <img src="./me.jpeg" alt="Shuvo Paul" />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
