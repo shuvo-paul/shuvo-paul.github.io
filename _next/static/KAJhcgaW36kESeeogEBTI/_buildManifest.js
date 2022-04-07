3aeb977ddccdbfc6a74a976a8212ec4e5db38c7e
shuvo-paul <shuvopaul@yahoo.com>
2022-04-07 12:42:28 +0000

    Deploying to gh-pages from @ shuvo-paul/shuvo-paul.github.io@45ce5967065ef13ccf66ceb846a6508086093aa8 🚀

--- _next/static/KAJhcgaW36kESeeogEBTI/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/652-130506a3355f185d0582.js","static/chunks/pages/index-36a5cb4c7137339896a1.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/jVNacnlTX_DgI6zX1sBBT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/652-130506a3355f185d0582.js","static/chunks/pages/index-54c6f639d129eb7df6f8.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
