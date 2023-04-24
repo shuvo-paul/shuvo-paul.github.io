83c8e9b5080c927cd056922a6bf298c0a6edf7cb
Shuvo Paul <shuvopaul@yahoo.com>
2023-04-25 00:08:38 +0600

    Experience update

--- src/lib/components/Experiences.svelte
+++ src/lib/components/Experiences.svelte
@@ -1,22 +1,38 @@
 <script>
     /**
-	 * @type {[{title: string, companyLink: string, company: string, year: string, content: string}]}
+	 * @type {[{title: string, companyLink: string, company: string, year: string, type: string, content: string}]}
 	 */
      export let list;
 </script>
 <section id="experience" class="mt-24 md:mt-28 lg:mt-32 relative z-10">
     <h2 class="text-2xl mb-9 font-heading-token">{`What I've experienced ...`} </h2>
     {#each list as item }
-        <div class="mb-8">
-            <div class="text-xl pl-8 relative before:w-[2px] before:h-full before:bg-primary/60 before:absolute before:left-0 before:top-1/2 before:-translate-y-1/2">
+        <div class="mb-8 max-w-xl">
+            <div class="text-xl pl-8 relative before:w-[2px] before:h-[22px] before:bg-primary/60 before:absolute before:left-0 before:top-0 before:mt-1">
                 {item.title}
-                    <a class="text-lg underline" target="_blank" href={`//${item.companyLink}`} rel="noreferrer"> @{item.company}</a> 
-                <div class=" text-sm"> ({item.year})</div>
+                <div class="flex items-end">
+                    <a class="text-base underline block" target="_blank" href={`//${item.companyLink}`} rel="noreferrer"> @{item.company}</a>
+                    {#if item.type}
+                        <div class="text-sm before:content-['-'] before:mx-1">
+                            {item.type}
+                        </div>
+                    {/if}
+                    <div class="text-sm before:content-['-'] before:mx-1">
+                        ({item.year})
+                    </div>
+                </div>
             </div>
             
-            <div class="mt-2 pl-8 ">
+            <div class="mt-2 pl-8 content">
                 {@html item.content}
             </div>
         </div>
     {/each}
 </section>
+
+<style lang="postcss" >
+
+    .content > :global(ul) > :global(li) {
+        @apply relative pl-6 before:content-['➪'] before:text-primary before:absolute before:left-0 before:top-0 font-light mb-1;
+    }
+</style>
---
+++ src/lib/md/experiences/01-nobrainer.md
@@ -0,0 +1,10 @@
+---
+title: "Developer"
+company: "Nobrainer"
+companyLink: "https://www.nobrainer.dk/"
+year: "Dec 2019 - Jun 2021"
+type: "Contract"
+---
+- Developed and styled website for clients. Mostly used Zurb Foundation, SCSS and JS.
+- Worked with SilverStripe CMS.
+- Colaborated with designer and other developers.
